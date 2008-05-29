07f156435a290e72f32cfb65d51c910f56682169
pavel.vinogradov <pavel.vinogradov@nixdev.net>
2008-05-29 17:55:31 +0000

    Add package to all source files

--- gsoc08-onto-swarm/ontology/examples/jontoheatbug/src/org/swarm/gsoc/ontology/apps/jheatbug/HeatbugModelSwarm.java
+++ gsoc08-onto-swarm/ontology/examples/jontoheatbug/src/org/swarm/gsoc/ontology/apps/jheatbug/HeatbugModelSwarm.java
@@ -1,6 +1,8 @@
+package org.swarm.gsoc.ontology.apps.jheatbug;
+
 // jheatbugs-3.0
 
-// Java Heatbugs application. Copyright © 1999-2000 Swarm Development Group.
+// Java Heatbugs application. Copyright ï¿½ 1999-2000 Swarm Development Group.
 // This library is distributed without any warranty; without even the
 // implied warranty of merchantability or fitness for a particular
 // purpose.  See file COPYING for details and terms of copying.
